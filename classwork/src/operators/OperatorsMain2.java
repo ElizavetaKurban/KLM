5afadba8f7a734c9e09179322369dc594bd906b4
Mac <Mac@MacBook-Pro-Mac.local>
2020-09-05 08:23:46 +0300

    first commit

---
+++ classwork/src/operators/OperatorsMain2.java
@@ -0,0 +1,15 @@
+package operators;
+
+public class OperatorsMain2 {
+    public static void main (String [] ar){
+        int a = 10;
+        long b = 5;
+        int c = a + (int) b;//плохо2
+
+        System.out.println(c);
+        System.out.println(10+5);
+        System.out.println(a+b);
+
+
+    }
+}
